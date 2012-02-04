8929b33337ba740bce9a550c67ad29a7aae554fd
Marcus Whybrow <marcus@marcuswhybrow.net>
2012-02-04 20:06:33 +0000

    Added an ant build file and a CraftBukkit plugin file.
    The ant file builds a jar containing the cbutils classes.

--- src/net/marcuswhybrow/minecraft/pvp/Plugin.java
+++ src/net/marcuswhybrow/minecraft/pvp/Plugin.java
@@ -1,19 +1,17 @@
 package net.marcuswhybrow.minecraft.pvp;
 
-import net.marcuswhybrow.minecraft.cbutils.MessageDispatcher;
-
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Plugin extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		MessageDispatcher.consoleInfo("[PVP Revolution] Started");
+		//TODO onEnable
 	}
 	
 	@Override
 	public void onDisable() {
-		MessageDispatcher.consoleInfo("[PVP Revolution] Stopped");
+		//TODO onDisable
+		
 	}
-
 }
