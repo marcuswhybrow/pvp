daf50028d8c6636e119da61e85bb7345d8a2bd9c
Marcus Whybrow <marcus@marcuswhybrow.net>
2012-02-04 17:13:24 +0000

    Added the basic JavaPlugin class which all CraftBukkit plugins require.

---
+++ src/net/marcuswhybrow/minecraft/pvp/Plugin.java
@@ -0,0 +1,19 @@
+package net.marcuswhybrow.minecraft.pvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
